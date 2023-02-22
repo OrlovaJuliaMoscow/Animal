929d7b0cca9f12c16bc4a21e3f7fc3d4a15e656c
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-23 01:40:32 +0300

    Добавьте файлы проекта.

---
+++ Animal/Animal_.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    abstract class Animal_
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        { Name = name; }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        //private string say;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+
+        }
+        public Cat (string name)//, string say)
+            : base(name)
+        // { this.say = say; }
+        public override void Say()
+        {
+            //Console.WriteLine(say);
+            Console.WriteLine("Miau");
+        }
+
+    }
+    class Dog : Animal
+    {
+        //private string say;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+
+        }
+        public Cat(string name)//, string say)
+            : base(name)
+       // { this.say = say; }
+        public override void Say()
+        {
+            //Console.WriteLine(say);
+            Console.WriteLine("Woof");
+        }
+
+}
+}
---
+++ Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog=new Dog("Bob");
+            Console.WriteLine(dog.Say());
+            Console.ReadKey();
+        }
+        
+    }
+}
